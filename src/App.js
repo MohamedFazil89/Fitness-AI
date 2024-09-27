8b536ff31146d9d8195aa13d9778844e08ef2b30
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-09-27 20:07:26 +0530

    starting element added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save! to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="SignUp-container">
+      <div className="Image-container">
+
+
+      </div>
+
+      <form action="/SignUp" method="POST" className="Form-container">
+      <h1 className="Title">Sign <span>up</span></h1>
+      <input 
+      type="text" 
+      placeholder="username" 
+      autoComplete="false"
+      className="input"
+      />
+
+      <input 
+      type="password" 
+      placeholder="password" 
+      autoComplete="false"
+      className="input"
+
+      />
+
+      <input 
+      type="password" 
+      placeholder="confirm password"
+      autoComplete="false"
+      className="input"
+
+       />
+
+      <input 
+      type="date"
+      placeholder="confirm password" 
+      autoComplete="false"
+      className="input-date"
+
+      />
+
+
+      <button type="submit" className="SignUp-btn">Sign Up</button>
+      <span className="Circle"></span>
+      <span className="Circle"></span>
+      <span className="Circle"></span>
+
+      <hr />
+      <p className="Already-Account">Already have an account? <span>Login</span></p>
+
+      </form>
+     
     </div>
   );
 }
