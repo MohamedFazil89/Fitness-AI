7cc591b4ccfa8750ccda35bcb06e6e54865e57cf
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-12-29 00:23:48 +0530

    ChatList - db data

--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -1,43 +1,54 @@
-import React, { useState } from 'react'
-import "./styles/ChatList.css"
-import Dummyusers from "./DummyUsers"
-
+import React, { useState, useEffect } from 'react';
+import './styles/ChatList.css';
+import axios from 'axios';
+import ProfilePic from '../assets/google.png';
 
 export default function ChatList() {
   const [SearchTerm, setSearchTerm] = useState('');
+  const [UserName, setUserName] = useState([]);
 
-  const filterUsers = Dummyusers.filter(user => user.Name.toLowerCase().includes(SearchTerm.toLowerCase()));
-  
+  // Filter usernames based on the search term
+  const filterUsers = UserName.filter((user) =>
+    user.toLowerCase().includes(SearchTerm.toLowerCase())
+  );
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/ChatList');
+        console.log(response.data.usernames);
+        setUserName(response.data.usernames); // Set usernames array
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className='ChatList-Container'>
+    <div className="ChatList-Container">
       <p>Messages</p>
-      <div className='ChatList-Header'>
+      <div className="ChatList-Header">
         <input
-         type="text" 
-         placeholder='Search'
-         value={SearchTerm}
-         onChange={e => setSearchTerm(e.target.value)}
-         
-          />
-      <div className='ChatList-Body'>
+          type="text"
+          placeholder="Search"
+          value={SearchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="ChatList-Body">
         {filterUsers.map((user, index) => (
-          <div key={index} className='ChatList-Item'>
-            <img src={user.profilepic} alt={user.Name} />
-            <div className='text-details'>
-              <h2>{user.Name}</h2>
-              <p>{user.LastMessage}</p>
+          <div key={index} className="ChatList-Item">
+            <img src={ProfilePic} alt={user} />
+            <div className="text-details">
+              <h2>{user}</h2>
+              <p>Last Message</p>
             </div>
             <button>Chat</button>
           </div>
         ))}
-
       </div>
-      </div>
-      
-
-      
     </div>
-  )
+  );
 }
